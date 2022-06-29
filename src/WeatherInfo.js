ff2ee47edb499f8892118c9119ea3aaadbd39008
MarijaTRastija <101635462+MarijaTRastija@users.noreply.github.com>
2022-06-29 18:46:18 +0200

    Icon and temperature position

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -26,7 +26,7 @@ export default function WeatherInfo(props) {
             <span className="unit">°C</span>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-6 air">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {Math.round(props.data.wind)} km/h</li>
