074c2f8ff5a9c44fd4fc2b8feea0d57d62be6981
MarijaTRastija <101635462+MarijaTRastija@users.noreply.github.com>
2022-06-29 09:29:58 +0200

    Styling of the App, but icon and temp need work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <Weather />
       <footer>
         This project was coded by Marija Tržić Rastija and is
         <a
